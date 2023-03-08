6eb07959299c1562c15d527962beed2d1842b0aa
noorfikri <s160419137@student.ubaya.ac.id>
2023-03-08 15:24:22 +0700

    Game Fragment Logic rough

--- app/src/main/java/com/ubaya/adv160419137week2/GameFragment.kt
+++ app/src/main/java/com/ubaya/adv160419137week2/GameFragment.kt
@@ -5,19 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.Navigation
 import com.google.android.material.textfield.TextInputEditText
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private var num1 = 0
+private var num2 = 0
+private var score = 0
 class GameFragment : Fragment() {
 
     override fun onCreateView(
@@ -29,11 +25,32 @@ class GameFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        generateNum(view)
+
         super.onViewCreated(view, savedInstanceState)
 
         if(arguments != null){
             val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
             view.findViewById<TextView>(R.id.txtTurn).text = "$playerName Turn"
         }
+
+        val btnAnswer = view.findViewById<Button>(R.id.btnAnswer)
+        btnAnswer.setOnClickListener {
+            var txtAnswer = view.findViewById<TextView>(R.id.txtAnswer).text.toString()
+            if(txtAnswer.toInt() == num1 + num2){
+                score++
+            }else{
+                val action = GameFragmentDirections.actionResultFragment(score)
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+    }
+
+    fun generateNum(view: View){
+        num1 = Random.nextInt(0,99)
+        num2 = Random.nextInt(0,99)
+
+        view.findViewById<TextView>(R.id.txtNum1).text = num1.toString()
+        view.findViewById<TextView>(R.id.txtNum2).text = num2.toString()
     }
 }
